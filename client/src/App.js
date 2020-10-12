2456a3bf98679035cadab48e8e0d51a498d2b555
bslzz <bishalkoid@gmail.com>
2020-10-12 21:22:48 +0300

    added chat form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './css/App.css';
 
-const App = () => {};
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  return (
+    <div className="App">
+      {!loggedIn ? (
+        <div className="logIn">
+          <div className="inputs">
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Room" />
+          </div>
+          <button>Enter Chat</button>
+        </div>
+      ) : (
+        <h1>you are logged in</h1>
+      )}
+    </div>
+  );
+};
 
 export default App;
