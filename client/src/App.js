c0bc7db8e329fb7065a5967879675b17c70d4a1b
bslzz <bishalkoid@gmail.com>
2020-10-13 18:31:09 +0300

    created socket connection from backend to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import './css/App.css';
+
+let socket;
+const CONNECTION_PORT = 'localhost:5000';
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+
+  const [room, setRoom] = useState('');
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, [CONNECTION_PORT]);
+
+  const connetToRoom = () => {
+    //whenever you want to send the data use socket.emit
+    //'join_room' used in the backend, room is the data
+    socket.emit('join_room', room);
+  };
+
   return (
     <div className="App">
       {!loggedIn ? (
         <div className="logIn">
           <div className="inputs">
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Room" />
+            <input
+              type="text"
+              placeholder="Name"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Room"
+              onChange={(e) => setRoom(e.target.value)}
+            />
           </div>
-          <button>Enter Chat</button>
+          <button onClick={connetToRoom}>Enter Chat</button>
         </div>
       ) : (
         <h1>you are logged in</h1>
